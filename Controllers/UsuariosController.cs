e71d54c9e17543e6d2fec1a5115350fa5face943
Regis Teixeira <regismarcondes2016@gmail.com>
2022-05-26 19:23:58 -0300

    correção de erros

---
+++ Controllers/UsuariosController.cs
@@ -0,0 +1,53 @@
+using RpgMvc.Models;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace RpgMvc.Controllers
+{
+    public class UsuariosController : Controller
+    {
+        public string uriBase = "http://localhost:5000/Usuarios/";
+
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View("CadastrarUsuario");
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> RegistrarAsync(UsuarioViewModel u)
+        {
+            try
+            {
+                HttpClient httpClient = new HttpClient();
+                string uriComplementar = "Registrar";
+
+                var content = new StringContent(JsonConvert.SerializeObject(u));
+                content.Headers.ContentType = new System.Net.Http.Headers.MediaTypeHeaderValue("application/json");
+                HttpResponseMessage response = await httpClient.PostAsync(uriBase + uriComplementar, content);
+
+                string serialized = await response.Content.ReadAsStringAsync();
+
+                if (response.StatusCode == System.Net.HttpStatusCode.OK)
+                {
+                    TempData["Mensagem"] =
+                        string.Format($"Usuário{u.Username} registrado com sucesso! Faça o login para acessar.");
+                    return View("AutenticarUsuario");
+                }
+                else
+                {
+                    throw new System.Exception(serialized);
+                }
+            }
+            catch (System.Exception ex)
+            {
+                TempData["MensagemErro"] = ex.Message;
+                return RedirectToAction("Index");
+            }
+        }
+    }
+}
