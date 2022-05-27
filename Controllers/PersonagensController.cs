bf7c4370d800c6ef0b8c72ac40a864ac4903f8a3
Régis Teixeira <regismarcondes2016@gmail.com>
2022-05-27 17:59:23 -0300

    adicionando funções

--- Controllers/PersonagensController.cs
+++ Controllers/PersonagensController.cs
@@ -75,7 +75,34 @@ namespace RpgMvc.Controllers
             [HttpGet]
             public ActionResult Create()
             {
-                return View();
+                return View(); 
+            }
+
+            [HttpGet]
+            public async Task<ActionResult> DetailsAsync(int? id)
+            {
+                try
+                {
+                    HttpClient httpClient = new HttpClient();
+                    string token = HttpContext.Session.GetString("SessionTokenUsuario");
+                    httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+                    HttpResponseMessage response = await httpClient.GetAsync(uriBase + id.ToString());
+                    string serialized = await response.Content.ReadAsStringAsync();
+
+                    if (response.StatusCode == System.Net.HttpStatusCode.OK)
+                    {
+                        PersonagemViewModel p = await Task.Run(() =>
+                        JsonConvert.DeserializeObject<PersonagemViewModel>(serialized));
+                        return View(p);
+                    }
+                    else   
+                        throw new System.Exception(serialized);
+                }
+                catch (System.Exception ex)
+                {
+                    TempData["MensagemErro"] = ex.Message;
+                    return RedirectToAction("Index");
+                }
             }
             
     }
